b3c1695e8cf4bc4b43d50526541e858594c2a11f
RIADEV <riadev@riadev.info>
2014-12-07 21:15:41 +0000

    docker update

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-forever forever start node_modules/.bin/actionhero
+/etc/init.d/redis-server start
+cd /opt/3dn/
+tail -f /var/log/redis/redis-server.log
+#forever  start node_modules/.bin/actionhero
+#forever logs 0
