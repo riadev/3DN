b928af310b0a09bab7bdf0d270a672eaf57bca5a
RIADEV <riadev@riadev.info>
2014-12-08 16:58:12 +0000

    update docker files

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 /etc/init.d/redis-server start
 /etc/init.d/rsyslog start
 forever -o >(logger -t dns_log) -e >(logger -t dns_error)  --sourceDir=/opt/riadns/ --workingDir=/opt/riadns/  node_modules/actionhero/bin/actionhero
